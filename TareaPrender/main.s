e9248a0f3575a433e166689da42bbb37ac687a43
Alessa1001 <91088661+Alessa1001@users.noreply.github.com>
2022-05-18 20:16:18 -0500

    Add files via upload

---
+++ TareaPrender/main.s
@@ -0,0 +1,42 @@
+SYSTICK				EQU 0XE000E000	
+SYSTICK_CTRL		EQU 0xE000E010	;direccion del registro CTRL de systick
+GPIO_PORTN_DATO  	EQU 0x40064001	;mascara del puerto c
+
+		
+
+		AREA |.text|, CODE, READONLY, ALIGN = 2
+		THUMB
+		
+		IMPORT ConfN
+		IMPORT prender
+		IMPORT apagar
+		EXPORT Start
+			
+Start
+		BL ConfN
+		MOV R8, #0x01
+		LDR R0, =GPIO_PORTN_DATO
+		STR R8, [R0]
+		BL prender
+		
+Interruptor
+		TST R8, #0x01
+		BEQ APAGA
+		B PRENDE
+		
+PRENDE
+		MOV R8, #0x01
+		LDR R0, =GPIO_PORTN_DATO
+		STR R8, [R0]
+		BL prender
+		B Interruptor	
+		
+APAGA
+		MOV R8, #0x00
+		LDR R0, =GPIO_PORTN_DATO
+		STR R8, [R0]
+		BL apagar
+		B Interruptor
+		
+		ALIGN 
+		END
